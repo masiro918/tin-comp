ed289713b7519368e864a27002eb3d1993e7f2d5
masiro918 <matias.siro@outlook.com>
2026-07-27 10:42:45 +0300

    update license terms

--- make_prod_build.sh
+++ make_prod_build.sh
@@ -6,3 +6,7 @@ cp stdlib.s dist/main/stdlib.s
 cd ..
 cp license.txt src/dist/main/license.txt
 mv src/dist/main/main src/dist/main/tincomp
+
+mkdir src/dist/main/THIRD_PARTY_LICENSES
+mv doc/licenses/cpython_license.txt src/dist/main/THIRD_PARTY_LICENSES/cpython_license.txt
+mv doc/licenses/py_installer_license.txt src/dist/main/THIRD_PARTY_LICENSES/py_installer_license.txt
